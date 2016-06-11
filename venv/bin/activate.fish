f861ca8ea773b1bec17de002e0a5403b93c407c3
Javiera Guedes <javierahermosa@yahoo.com>
2016-06-11 22:01:33 +0200

    new

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -32,7 +32,7 @@ end
 # unset irrelevant variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/jguedes/Apps/Kurt/venv"
+set -gx VIRTUAL_ENV "/Users/jguedes/Apps/KurtsApp/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
